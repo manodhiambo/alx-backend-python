19b7777c23cc4b68c1f98161a0f761bb9e50def9
manodhiambo <manodhiambo@gmail.com>
2022-06-28 04:36:29 +0000

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Async Comprehensions
+"""
+
+from typing import List
+import asyncio
+import random
+async_generator = __import__('0-async_generator.py').async_generator
+
+
+async def async_comprehension() -> List[float]:
+"""
+collects 10 rand nums using comprehension
+"""
+
+return [i async for i in async_generator()]
